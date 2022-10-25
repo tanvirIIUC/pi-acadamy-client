32d47882e1508c73c74998ea41b6a909ae2ba812
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-10-25 15:26:35 +0600

    route add

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+
+export const routes = createBrowserRouter([
+
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            
+        ]
+    }
+])
